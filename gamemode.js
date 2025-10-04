de51641bdee7779fe49407bbeb0139d0e4d1c9de
4gad1s <temirbay.agadil@gmail.com>
2025-10-04 13:45:58 +0500

    Update gamemode.js

--- gamemode.js
+++ gamemode.js
@@ -9,10 +9,10 @@ const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
 export function game_mode_length_seconds() {
     const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
     switch (length) {
-        case 'Length_S': return 240; // 4 min
-        case 'Length_M': return 300; // 5 min
-        case 'Length_L': return 360; // 6 min
-        case 'Length_XL': return 420; // 7 min
+        case '1h': return 3600; // 4 min
+        case '2h': return 7200; // 5 min
+        case '3h': return 10800; // 6 min
+        case '5h': return 18000; // 7 min
     }
     return 300;
 }
