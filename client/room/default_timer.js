8923a2f359f6649612b05eeca06d3bfa425d464e
4gad1s <temirbay.agadil@gmail.com>
2025-10-05 20:05:09 +0500

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -5,14 +5,18 @@ import { GameMode } from 'pixel_combats/room';
 // константы
 const PARAMETER_GAME_LENGTH = 'default_game_mode_length';
 
-// возвращает длину матча
+// возвращает длину матча в секундах
 export function game_mode_length_seconds() {
     const length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
     switch (length) {
-        case '1h': return 3600; // 4 min
-        case '2h': return 7200; // 5 min
-        case '3h': return 10800; // 6 min
-        case '5h': return 18000; // 7 min
+        case '1h': return 3600;
+        case '2h': return 7200;
+        case '3h': return 10800;
+        case '5h': return 18000;
+        case '∞':
+        case 'infinite':
+        case 'none':
+            return Infinity; // бесконечное время
     }
-    return 300;
+    return Infinity;
 }
